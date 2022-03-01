52ee6696fe64b112ba6a5b578caf01638d6fa1aa
Eric Hoanshelt <eric.hoanshelt@wpengine.com>
2022-03-01 14:39:24 -0500

    home page set

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './public/home-page/home-page.component';
 
 const routes: Routes = [
   { path:'games', 
   loadChildren: () => import('./games/games.module').then((m) => m.GamesModule)},
+  { path: '', component:HomePageComponent}
 ];
 
 @NgModule({
